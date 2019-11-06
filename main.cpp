94071813231e854ac582dc59a6b4662e4df20a5b
misbahq <57337568+misbahq@users.noreply.github.com>
2019-11-05 22:38:52 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//FizzBuzz in C++ by Misbah Qureshi 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  for(int i=1; i<=100; i++){
+      if((i%3==0)&&(i%5==0))
+         cout<<"Fizzbuzz\n";
+      else if(i%3==0)
+         cout<<"Fizz\n"; 
+      else if(i%5==0)
+         cout<<"Buzz\n";
+      else 
+         cout<<i<<"\n";
+   }
+} 
